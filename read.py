9ea927829f18171623db6609705be9601a97cc2c
zRyyH <zryyh.br@gmail.com>
2026-02-06 20:56:16 -0300

    v1.0

--- read.py
+++
@@ -1,9 +0,0 @@
-from pocket import create_account
-from utils import logger
-import pandas as pd
-
-df = pd.read_excel("accs.xlsx", sheet_name="Planilha1")
-
-for index, row in df.iterrows():
-    create_account({"username": row.tolist()[0], "password": row.tolist()[1]})
-    logger.info(f"Conta criada: {index}")
